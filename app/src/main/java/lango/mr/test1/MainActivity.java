9876b41c570e237e0e1e8bac4d921ebc714f3433
MrLango <shourya0302@gmail.com>
2018-02-24 23:57:36 +0530

    initial

--- app/src/main/java/lango/mr/test1/MainActivity.java
+++ app/src/main/java/lango/mr/test1/MainActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b=(Button)findViewById(R.id.button);
+        b=(Button)findViewById(R.id.button);
     }
-    
+
 }
